c586d2946ce654f55ed63a1ce3df199fc5b372eb
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-10-26 16:27:13 +0530

    Setup : Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx'
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import Home from "./components/Home.jsx";
+import Error from "./components/Error.jsx";
+import Verify from "./components/Verify.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "/verify",
+        element: <Verify />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
